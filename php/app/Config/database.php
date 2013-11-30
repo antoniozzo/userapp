5ccf061edab8765f65ef0b0aaa815fb6cbc09717
Antonio Rizzo <antonio@creativefingerprints.com>
2013-11-30 23:07:12 +0100

    database

--- php/app/Config/database.php
+++ php/app/Config/database.php
@@ -25,4 +25,14 @@ class DATABASE_CONFIG {
 		'prefix' => '',
 		'encoding' => 'utf8',
 	);
+
+	public function __construct() {
+		if (getenv("OPENSHIFT_MYSQL_DB_HOST")) {
+			$this->default['host']     = getenv("OPENSHIFT_MYSQL_DB_HOST");
+			$this->default['port']     = getenv("OPENSHIFT_MYSQL_DB_PORT");
+			$this->default['login']    = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+			$this->default['password'] = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+			$this->default['database'] = getenv("OPENSHIFT_APP_NAME");
+		}
+	}
 }
